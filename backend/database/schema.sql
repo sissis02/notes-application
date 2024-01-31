a764eda7d2be335e42f27f7ca5aab6c6a6de966a
sissis02 <sissis1993@gmail.com>
2024-01-31 09:32:28 +0100

    create database

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,4 +1,23 @@
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS note;
+--example
 create table item (
   id int unsigned primary key auto_increment not null,
   title varchar(255) not null
+);
+-- example
+
+CREATE TABLE user (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  firstname VARCHAR(100) NOT NULL,
+  lastname VARCHAR(100) NOT NULL,
+  password VARCHAR(500) NOT NULL
+);
+
+CREATE TABLE note (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR(100) NOT NULL,
+  description TEXT NOT NULL,
+  user_id INT,
+  CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
 );
